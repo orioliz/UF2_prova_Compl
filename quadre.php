75237298363e0d64e0fac950f278c101b44b4acb
orioliz <oriolizquierdovi@gmail.com>
2017-02-13 17:53:17 +0100

    oriol

--- quadre.php
+++ quadre.php
@@ -1,1 +1,17 @@
-quadre.php
+<?php
+
+
+class Quadre {
+
+	protected $color;
+
+	function __construct($solor) {
+		$this->color=$color;
+	}
+
+
+
+	function show() {
+		echo '<td style="padding-left:2px;background-color:'$this->.'">'.'<div style="height:50px; overflow:hidden"></div></td>';
+	}
+}
--- tauler.php
+++ tauler.php
@@ -35,18 +35,4 @@ class Tauler {
 
 
 
-class Quadre {
-
-	protected $color;
-
-	function __construct($solor) {
-		$this->color=$color;
-	}
-
-
-
-	function show() {
-		echo '<td style="padding-left:2px;background-color:'$this->.'">'.'<div style="height:50px; overflow:hidden"></div></td>';
-	}
-}
 
